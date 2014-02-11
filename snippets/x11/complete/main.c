3f51e76a3a6a51494db30fd557907169f3f0d724
jgvictores <jcgvicto@gmail.com>
2014-02-11 19:24:07 +0100

    xlib (X11) examples

---
+++ snippets/x11/complete/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+#include <X11/Xatom.h>
+#include <X11/keysym.h>
+
+int main() {
+	Display *dis;
+    dis = XOpenDisplay(NULL);
+	if (!dis) {fprintf(stderr, "unable to connect to display\n");return 7;}
+	Window win;
+    win = XCreateSimpleWindow(dis, RootWindow(dis, 0), 1, 1, 500, 500, \
+    0, BlackPixel (dis, 0), BlackPixel(dis, 0));
+	/* tell the display server what kind of events we would like to see */
+	XSelectInput(dis, win, ButtonPressMask|StructureNotifyMask|KeyPressMask|KeyReleaseMask|KeymapStateMask);
+
+    XMapWindow(dis, win);
+    XFlush(dis);
+    XEvent report;
+    while (1) {
+        XNextEvent(dis, &report);
+        switch  (report.type) {
+            case KeyPress:
+                printf("A key was pressed.\n");
+                if (XLookupKeysym(&report.xkey, 0) == XK_space)  {
+                    fprintf (stdout, "The space bar was pressed.\n");
+                }
+            //case Expose:   
+            //    fprintf(stdout, "I have been exposed.\n");
+            /*A local program function to redraw the window should be called.*/
+            default: 
+                break;
+        }
+    }
+    /*Sleep 5 seconds before closing.*/
+    // sleep(5);
+    return(0);
+}
+
