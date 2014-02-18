3403010ec6a162d5d01cd6a36c6e7630132015be
David-Estevez <david.estevez.fdez@gmail.com>
2014-02-18 19:33:11 +0100

    Added RdCameraLib Skeleton.

---
+++ src/libraries/RdCameraLib/RdCameraBase.hpp
@@ -0,0 +1,46 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __RD_CAMERA_BASE_HPP__
+#define __RD_CAMERA_BASE_HPP__
+
+#include <iostream>
+#include "RdManagerBase.hpp"
+
+/**
+ * @ingroup rd_libraries
+ *
+ * \defgroup RdCameraLib
+ *
+ * @brief The RdCameraLib library contains the \ref RdCameraBase class and some inherited implementations.
+ */
+
+namespace rdlib{
+
+class RdManagerBase;
+class RdRobotBase;
+
+/**
+ * @ingroup RdCameraBase
+ *
+ * @brief An abstract base class for Robot Devastation camera inputs (webcams, stereoscopic cams, etc).
+ *
+ */
+class RdCameraBase {
+    public:
+        /** An init rutine.
+         * @return true if the object was init successfully.
+         */
+        virtual bool stop() = 0;
+
+
+        void setRdManagerBasePtr(RdManagerBase* rdManagerBasePtr ) {
+            this->rdManagerBasePtr = rdManagerBasePtr;
+        }
+
+    protected:
+        RdManagerBase* rdManagerBasePtr;
+};
+
+} //rdlib
+
+#endif  // __RD_CAMERA_BASE_HPP__
