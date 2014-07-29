fe63e78e2b5100039624eb6862727d5943327a70
jgvictores <jcgvicto@gmail.com>
2014-07-29 16:56:26 +0200

    more info on login

--- src/programs/robotDevastation/RobotDevastation.cpp
+++ src/programs/robotDevastation/RobotDevastation.cpp
@@ -68,6 +68,7 @@ bool rd::RobotDevastation::configure(yarp::os::ResourceFinder &rf)
         if(rpcClient.getOutputCount() > 0) break;
         printf("Waiting to be connected to server...\n");
         yarp::os::Time::delay(0.5);
+        yarp::os::Network::connect( ("/rpc/"+s.str()).c_str() , "/rdServer" );
     }
 
     yarp::os::Bottle msgRdPlayer,res;
@@ -76,7 +77,7 @@ bool rd::RobotDevastation::configure(yarp::os::ResourceFinder &rf)
     msgRdPlayer.addString(rdPlayer.getName().c_str());
     msgRdPlayer.addInt(rdPlayer.getTeam_id());
     rpcClient.write(msgRdPlayer,res);
-
+    RD_INFO("rdServer response from login: %s",res.toString().c_str());
     return true;
 }
 
