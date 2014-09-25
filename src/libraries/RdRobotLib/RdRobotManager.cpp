1772109517ce4cce3296091d7c02b7f68a07aba3
David-Estevez <david.estevez.fdez@gmail.com>
2014-09-25 16:10:04 +0200

    Add **simple** RdRobotManager interface

---
+++ src/libraries/RdRobotLib/RdRobotManager.cpp
@@ -0,0 +1,63 @@
+#include "RdRobotManager.hpp"
+
+//-- This is very important:
+rd::RdRobotManager * rd::RdRobotManager::robotManager = NULL;
+
+
+void rd::RdRobotManager::registerManager(std::string name, rd::RdRobotManager *manager)
+{
+    if ( robotManagerRegistry.find(name) == robotManagerRegistry.end() )
+    {
+        robotManagerRegistry[name] = manager;
+    }
+    else
+    {
+        RD_ERROR("RdManager with name \"%s\" already exists!\n", name.c_str());
+    }
+}
+
+std::vector<std::string> rd::RdRobotManager::listAll()
+{
+    std::vector<std::string> robot_manager_list;
+
+    for( std::map<std::string, RdRobotManager*>::const_iterator it = robotManagerRegistry.begin(); it != robotManagerRegistry.end(); ++it)
+    {
+        robot_manager_list.push_back(it->first);
+    }
+
+    return robot_manager_list;
+}
+
+rd::RdRobotManager *rd::RdRobotManager::getRobotManager(std::string name)
+{
+    if (robotManager == NULL)
+    {
+        if ( robotManagerRegistry.find(name) != robotManagerRegistry.end() )
+        {
+            robotManager = robotManagerRegistry[name];
+        }
+        else
+        {
+            RD_ERROR("RdRobotManager requested (\"%s\") does not exist.\n", name.c_str());
+        }
+    }
+
+    return robotManager;
+}
+
+bool rd::RdRobotManager::destroyRobotManager()
+{
+    if (robotManager == NULL)
+        return false;
+
+    delete robotManager;
+    robotManager = NULL;
+
+    return true;
+}
+
+
+rd::RdRobotManager::~RdRobotManager()
+{
+    this->onDestroy();
+}
---
+++ src/libraries/RdRobotLib/RdRobotManager.hpp
@@ -0,0 +1,48 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __RD_ROBOT_MANAGER_HPP__
+#define __RD_ROBOT_MANAGER_HPP__
+
+#include "RdMacros.hpp"
+#include <string>
+#include <vector>
+#include <map>
+
+namespace rd{
+
+class RdRobotManager
+{
+    public:
+        //-- Robot movement related functions    
+        virtual bool moveForward(int time = UNUSED, int velocity = UNUSED) = 0;
+        virtual bool moveBackwards(int time = UNUSED, int velocity = UNUSED) = 0;
+        virtual bool turnLeft(int time = UNUSED, int velocity = UNUSED) = 0;
+        virtual bool turnRight(int time = UNUSED, int velocity = UNUSED) = 0;
+
+        //-- Robot camera related functions
+        virtual bool tiltUp(int time = UNUSED, int velocity = UNUSED) = 0;
+        virtual bool tiltDown(int time = UNUSED, int velocity = UNUSED) = 0;
+        virtual bool panLeft(int time = UNUSED, int velocity = UNUSED) = 0;
+        virtual bool panRight(int time = UNUSED, int velocity = UNUSED) = 0;
+        
+        //-- Robot connection related functions
+        virtual bool connect() = 0;
+        virtual bool disconnect() = 0;
+        virtual bool test() = 0;
+        virtual bool ping() = 0;
+
+        //-- Other
+        virtual void onDestroy() = 0;
+        ~RdRobotManager();
+
+        //-- Constants
+        static const int UNUSED = -1;
+
+        RdRobotManager();
+};
+
+}
+
+
+
+#endif //-- __RD_ROBOT_MANAGER_HPP__
