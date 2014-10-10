90686bdaad9474f40e828fb8077c234818cda05d
jgvictores <jcgvicto@gmail.com>
2014-10-10 14:22:03 +0200

    preparing for RdStateMachine

---
+++ src/libraries/RdStateMachineLib/RdStateMachine.hpp
@@ -0,0 +1,15 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __RD_STATE_MACHINE_HPP__
+#define __RD_STATE_MACHINE_HPP__
+
+namespace rd{
+
+class RdStateMachine {
+
+};
+
+}
+
+#endif //-- __RD_STATE_MACHINE_HPP__
+
