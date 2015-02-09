b21050fed6628a663d590d934a231ecee737abd0
David-Estevez <david.estevez.fdez@gmail.com>
2015-02-09 19:06:22 +0100

    Add skeleton of MockupNetworkManager.

---
+++ src/libraries/RdNetworkLib/MockupNetworkManager.cpp
@@ -0,0 +1,74 @@
+#include "MockupNetworkManager.hpp"
+
+//-- Initialize static members
+rd::MockupNetworkManager * rd::MockupNetworkManager::uniqueInstance = NULL;
+const std::string rd::MockupNetworkManager::id = "MOCKUP";
+
+
+bool rd::MockupNetworkManager::RegisterManager()
+{
+    if (uniqueInstance == NULL)
+    {
+        uniqueInstance = new MockupNetworkManager();
+    }
+
+    return Register( uniqueInstance, id);
+}
+
+rd::MockupNetworkManager::~MockupNetworkManager()
+{
+    uniqueInstance = NULL;
+}
+
+bool rd::MockupNetworkManager::start()
+{
+    stopped = false;
+}
+
+bool rd::MockupNetworkManager::stop()
+{
+    stopped = true;
+}
+
+bool rd::MockupNetworkManager::sendPlayerHit(rd::RdPlayer player, int damage)
+{
+    return false;
+}
+
+bool rd::MockupNetworkManager::login(rd::RdPlayer player)
+{
+    return false;
+}
+
+bool rd::MockupNetworkManager::logout(rd::RdPlayer player)
+{
+    return false;
+}
+
+bool rd::MockupNetworkManager::isLoggedIn()
+{
+    return logged_in;
+}
+
+bool rd::MockupNetworkManager::isStopped()
+{
+    return stopped;
+}
+
+bool rd::MockupNetworkManager::setPlayerData(std::vector<rd::RdPlayer> players)
+{
+    this->players = players;
+}
+
+std::vector<rd::RdPlayer> rd::MockupNetworkManager::getPlayerData()
+{
+    return players;
+}
+
+bool rd::MockupNetworkManager::sendPlayerData()
+{
+    //-- Notify listeners
+    for(int i = 0; i < listeners.size(); i++)
+        listeners[i]->onDataArrived(players);
+}
+
---
+++ src/libraries/RdNetworkLib/MockupNetworkManager.hpp
@@ -0,0 +1,90 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __MOCKUP_NETWORK_MANAGER_HPP__
+#define __MOCKUP_NETWORK_MANAGER_HPP__
+
+#include "RdUtils.hpp"
+#include "RdNetworkManager.hpp"
+#include "RdNetworkEventListener.hpp"
+#include "RdPlayer.hpp"
+
+namespace rd{
+
+/**
+ * @ingroup RdNetworkLib
+ *
+ * @brief Allow for low-level access of members in tests / manipulating internal data
+ *
+ * MockupNetworkManager is a <a href="http://en.wikipedia.org/wiki/Singleton_pattern">singleton text</a> (only
+ * one instance of this object can exist, that is is shared by all the users). To use this
+ * class, we first get the reference to the MockupNetworkManager with getNetworkManager() and then we
+ * access the manager with that reference.
+ *
+ * When the program finishes, the MockupNetworkManager can be deallocated using destroyNetworkManager().
+ *
+ * Network events are broadcasted to the registered <a href="http://en.wikipedia.org/wiki/Observer_pattern">listeners</a>,
+ * along with the data relevant to the event triggered (i.e. data that just arrived)
+ *
+ */
+
+class MockupNetworkManager : RdNetworkManager
+{
+    public:
+        //-- Creation and configuration
+        //--------------------------------------------------------------------------------------------
+        /**
+         * @brief Register this manager in the RdNetworkManager registry so that can be used
+         *
+         * It ensures that only one manager of this type is created (unique instance)
+         */
+        static bool RegisterManager();
+
+        ~MockupNetworkManager();
+
+        //! @brief String that identifies this manager
+        static const std::string id;
+
+        //------------------------------ Manager Startup & Halting ----------------------------------------------------//
+        virtual bool start();
+        virtual bool stop();
+
+        //! @brief Configures a parameter with a value
+        virtual bool configure(std::string parameter, std::string value);
+
+        //-- RdServer API
+        //--------------------------------------------------------------------------------------------
+        virtual bool sendPlayerHit(RdPlayer player, int damage);
+        virtual bool login(RdPlayer player);
+        virtual bool logout(RdPlayer player);
+
+        //-- Mockup object API
+        //--------------------------------------------------------------------------------------------
+        bool isLoggedIn();
+        bool isStopped();
+
+        bool setPlayerData(std::vector<RdPlayer> players);
+        std::vector<RdPlayer> getPlayerData();
+        bool sendPlayerData();
+
+
+    private:
+        /**
+         * @brief Constructor
+         *
+         * Constructor for this class is private, since the singleton can only be instantiated once,
+         * and the instantiation is done at the getNetworkManager() method
+         */
+        MockupNetworkManager();
+
+        //! @brief Reference to this manager (unique instance)
+        static MockupNetworkManager * uniqueInstance;
+
+        //-- Required stuff
+        bool logged_in;
+        bool stopped;
+        std::vector<RdPlayer> players;
+
+};
+}
+
+#endif //-- __RD_NETWORK_MANAGER_HPP__
