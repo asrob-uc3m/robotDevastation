bcc241f243d6c220b3effb62fec3851eba3b0b5b
David-Estevez <david.estevez.fdez@gmail.com>
2015-03-15 14:34:14 +0100

    Create test file, tried to solve issues with ManagerHub
    ManagerHub returns errors when compiling, telling that
    RdMentalMap (and RdWeapon) and RdInputManager (and RdKey)
    are not declared (when they are).

---
+++ src/libraries/RdUtilsLib/Hub.cpp
@@ -0,0 +1,25 @@
+#include "Hub.hpp"
+
+rd::ManagerHub::ManagerHub()
+{
+    networkManager = NULL;
+    imageManager = NULL;
+    inputManager = NULL;
+    mentalMap = NULL;
+    robotManager = NULL;
+}
+
+rd::ManagerHub::ManagerHub(rd::RdNetworkManager *networkManager, rd::RdImageManager *imageManager, rd::RdInputManager *inputManager, rd::RdMentalMap *mentalMap, rd::RdRobotManager *robotManager)
+{
+    this->networkManager = networkManager;
+    this->imageManager = imageManager;
+    this->inputManager = inputManager;
+    this->mentalMap = mentalMap;
+    this->robotManager = robotManager;
+}
+
+bool rd::ManagerHub::setNetworkManager(rd::RdNetworkManager *networkManager) { this->networkManager = networkManager;}
+bool rd::ManagerHub::setImageManager(rd::RdImageManager *imageManager) { this->imageManager = imageManager;}
+bool rd::ManagerHub::setInputManager(rd::RdInputManager *inputManager) { this->inputManager = inputManager;}
+bool rd::ManagerHub::setMentalMap(rd::RdMentalMap *mentalMap) { this->mentalMap = mentalMap;}
+bool rd::ManagerHub::setRobotManager(rd::RdRobotManager *robotManager) { this->robotManager = robotManager;}
--- src/libraries/RdUtilsLib/Hub.hpp
+++ src/libraries/RdUtilsLib/Hub.hpp
@@ -7,14 +7,16 @@
 #include "RdImageManager.hpp"
 #include "RdAudioManager.hpp"
 #include "RdInputManager.hpp"
+#include "RdKey.hpp"
 #include "RdMentalMap.hpp"
+#include "RdWeapon.hpp"
 #include "RdNetworkManager.hpp"
 #include "RdRobotManager.hpp"
 
 namespace rd{
 
 /**
- * @ingroup RdStateMachineLib
+ * @ingroup RdUtils
  *
  * @brief A classs to interface all the robot devastation managers
  *
@@ -22,29 +24,16 @@ namespace rd{
 class ManagerHub
 {
     public:
-        ManagerHub()
-        {
-            networkManager = NULL;
-            imageManager = NULL;
-            inputManager = NULL;
-            mentalMap = NULL;
-            robotManager = NULL;
-        }
+    ManagerHub();
 
-        ManagerHub(RdNetworkManager * networkManager, RdImageManager * imageManager, RdInputManager * inputManager, RdMentalMap * mentalMap, RdRobotManager * robotManager)
-        {
-            this->networkManager = networkManager;
-            this->imageManager = imageManager;
-            this->inputManager = inputManager;
-            this->mentalMap = mentalMap;
-            this->robotManager = robotManager;
-        }
+    ManagerHub(RdNetworkManager * networkManager, RdImageManager * imageManager,
+               RdInputManager * inputManager, RdMentalMap * mentalMap, RdRobotManager * robotManager);
 
-        bool setNetworkManager(RdNetworkManager * networkManager) { this->networkManager = networkManager;}
-        bool setImageManager(RdImageManager * imageManager) { this->imageManager = imageManager;}
-        bool setInputManager(RdInputManager * inputManager) { this->inputManager = inputManager;}
-        bool setMentalMap(RdMentalMap * mentalMap) { this->mentalMap = mentalMap;}
-        bool setRobotManager(RdRobotManager * robotManager) { this->robotManager = robotManager;}
+        bool setNetworkManager(RdNetworkManager * networkManager);
+        bool setImageManager(RdImageManager * imageManager);
+        bool setInputManager(RdInputManager * inputManager);
+        bool setMentalMap(RdMentalMap * mentalMap);
+        bool setRobotManager(RdRobotManager * robotManager);
 
    protected:
         //-- Pointers to everything important in the game:
@@ -55,6 +44,11 @@ class ManagerHub
         RdRobotManager * robotManager;
 
 };
+
+
+
+
+
 }
 
 #endif //-- __MANAGER_HUB_HPP__
