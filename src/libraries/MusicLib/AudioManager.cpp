4fb3a481a7bb258e17620c48584311eee29a71fc
David-Estevez <david.estevez.fdez@gmail.com>
2015-03-28 23:30:42 +0100

    Create generic AudioManager class

---
+++ src/libraries/MusicLib/AudioManager.cpp
@@ -0,0 +1,112 @@
+#include "AudioManager.hpp"
+
+//-- This is very important:
+rd::AudioManager * rd::AudioManager::audioManagerInstance = NULL;
+std::string rd::AudioManager::currentId = "";
+std::map<std::string, rd::AudioManager *> rd::AudioManager::audioManagerRegistry = std::map<std::string, rd::AudioManager *>();
+
+rd::AudioManager *rd::AudioManager::getAudioManager()
+{
+    if (audioManagerInstance == NULL )
+    {
+        if(!audioManagerRegistry.empty() )
+        {
+            //-- Install the first RdImageManger found
+            currentId = audioManagerRegistry.begin()->first;
+            audioManagerInstance = audioManagerRegistry.begin()->second;
+        }
+        else
+        {
+            RD_ERROR("No AudioManager registered!! Returning null\n");
+        }
+    }
+
+    return audioManagerInstance;
+}
+
+rd::AudioManager *rd::AudioManager::getAudioManager(std::string id)
+{
+    if (audioManagerInstance == NULL )
+    {
+        if( audioManagerRegistry.find(id) != audioManagerRegistry.end() )
+        {
+            currentId = id;
+            audioManagerInstance = audioManagerRegistry[id];
+        }
+        else
+        {
+            RD_ERROR("AudioManager %s not found! (Maybe it was not registered?)\n", id.c_str());
+        }
+    }
+
+    return audioManagerInstance;
+}
+
+bool rd::AudioManager::destroyAudioManager()
+{
+    //-- First, stop & delete the manager currently in use (if any)
+    if (audioManagerInstance != NULL)
+    {
+        audioManagerInstance->stop();
+
+        delete audioManagerInstance;
+        audioManagerInstance = NULL;
+
+        //-- Remove it also from the registry
+        std::map<std::string, AudioManager *>::iterator it = audioManagerRegistry.find(currentId);
+        if (it != audioManagerRegistry.end())
+            audioManagerRegistry.erase(it);
+    }
+
+    //-- Destroy all the remaining registered InputManagers
+    for ( std::map<std::string, AudioManager *>::iterator it = audioManagerRegistry.begin();
+          it != audioManagerRegistry.end(); ++it)
+    {
+        AudioManager * currentManager = it->second;
+
+        if (currentManager != NULL)
+        {
+            currentManager->stop();
+
+            delete currentManager;
+            currentManager = NULL;
+         }
+
+         //-- Remove it also from the registry
+         audioManagerRegistry.erase(it);
+    }
+
+    return true;
+}
+
+rd::AudioManager::~AudioManager()
+{
+}
+
+bool rd::AudioManager::configure(std::string parameter, std::string value)
+{
+    return true;
+}
+
+bool rd::AudioManager::Register(rd::AudioManager *manager, std::string id)
+{
+    if ( audioManagerRegistry.find(id) == audioManagerRegistry.end())
+    {
+        if (manager != NULL)
+        {
+            audioManagerRegistry[id] = manager;
+            return true;
+        }
+        else
+        {
+            RD_ERROR("Trying to register a NULL manager with id \"%s\"\n", id.c_str());
+            return false;
+        }
+    }
+    else
+    {
+        RD_ERROR( "AudioManager with id \"%s\" was already registered\n", id.c_str());
+        return false;
+    }
+
+}
---
+++ src/libraries/MusicLib/AudioManager.hpp
@@ -0,0 +1,130 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __AUDIO_MANAGER_HPP__
+#define __AUDIO_MANAGER_HPP__
+
+#include <iostream>
+#include <map>
+
+#include "RdMacros.hpp"
+
+namespace rd{
+
+/**
+ * @ingroup rd_libraries
+ *
+ * \defgroup RdMusicLib
+ *
+ * @brief The RdMusicLib library contains classes related to game music and sound effects
+
+/**
+ * @ingroup RdMusicLib
+ *
+ * @brief Music and sound effects manager
+ *
+ * RdAudioManger can load several audio files and assign them a string for identification,
+ * so that they can be played later.
+ *
+ * RdAudioManager is a <a href="http://en.wikipedia.org/wiki/Singleton_pattern">singleton text</a> (only
+ * one instance of this object can exist, that is is shared by all the users). To use this
+ * class, we first get the reference to the RdAudioManager with getAudioManager() and then we
+ * access the manager with that reference.
+ *
+ * When the program finishes, the RdAudioManager can be deallocated using destroAudioManager().
+ */
+class AudioManager
+{
+    public:
+        //------------------------------ Construction & destruction ---------------------------------------------------//
+        /**
+         * @brief Get a reference to the AudioManager
+         * @return By default, if no id is specified, this will return a reference to the first
+         * AudioManager that it can find in the registry, or NULL if no AudioManager was registered.
+         */
+        static AudioManager * getAudioManager();
+
+        /**
+         * @brief Get a reference to the AudioManager
+         * @return The AudioManager registered with the given id, NULL if the id is not found in
+         * the registry.
+         */
+        static AudioManager * getAudioManager(std::string id);
+
+        //! @brief Deallocate all the registered RdInputManager
+        static bool destroyAudioManager();
+
+        virtual ~AudioManager();
+
+
+        //------------------------------ Manager Startup & Halting ----------------------------------------------------//
+        /**
+         * @brief Start the AudioManager
+         *
+         * This function is supposed to be called after AudioManager configuration.
+         */
+        virtual bool start() = 0;
+
+        //! @brief Stops the AudioManager
+        virtual bool stop() = 0;
+
+        //------------------------------- Audio Manager functions -----------------------------------------------------//
+        /**
+         * @brief Loads an audio file, assigning it a string as identifier
+         * @param music_filepath Path to the file to be loaded
+         * @param id String that will identify the audio loaded
+         * @param type Can take a value of MUSIC or FX depending if the audio is to be loaded as game music or
+         * sound effect
+         * @return True if sound was loaded successfully, false otherwise
+         */
+        virtual bool load( const std::string& music_filepath, const std::string& id, const int& type) = 0;
+
+        /**
+         * @brief Plays a music file previously loaded
+         * @param id String that identifies the music track
+         * @param loop Number of times the music is played. -1 means play the music forever.
+         * @return True if sound was played successfully, false otherwise
+         */
+        virtual bool playMusic(const std::string& id, int loop) = 0;
+
+        /**
+         * @brief Plays a sound effect file previously loaded
+         * @param id String that identifies the sound effect track
+         * @param loop Number of times the effect is played. -1 means play the effect forever.
+         * @return True if sound was played successfully, false otherwise
+         */
+        virtual bool playSound(const std::string& id, int loop) = 0;
+
+        /**
+         * @brief Stops the music being played currently
+         * @return False if the music could not be stopped
+         */
+        virtual bool stopMusic() = 0;
+
+
+        //------------------------------ Configuration ----------------------------------------------------------------//
+        //! @brief Configures a parameter with a value
+        virtual bool configure(std::string parameter, std::string value);
+
+    protected:
+        /**
+         * @brief This function allows subclasses to install their unique instances in the singleton register to be
+         * selected later by the user
+         */
+        static bool Register( AudioManager * manager, std::string id);
+
+
+    private:
+        //! @brief Stores the unique instance of the AudioManager
+        static AudioManager * audioManagerInstance;
+
+        //! \brief Stores the id of the current unique instance used
+        static std::string currentId;
+
+        //! \brief Stores all the RdInputManager that have been registered
+        static std::map< std::string, AudioManager * > audioManagerRegistry;
+
+};
+
+}
+
+#endif //-- __AUDIO_MANAGER_HPP__
