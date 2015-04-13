424700636c689ff31a3b277eb6fbd1c9d46f675d
David-Estevez <david.estevez.fdez@gmail.com>
2015-04-13 19:28:30 +0200

    Add mockup objects to test

--- test/testInitState.cpp
+++ test/testInitState.cpp
@@ -5,6 +5,14 @@
 #include "StateMachine.hpp"
 #include "StateMachineBuilder.hpp"
 #include "RdUtils.hpp"
+#include "InitState.hpp"
+
+#include "MockupNetworkManager.hpp"
+#include "RdMockupImageManager.hpp"
+#include "MockupInputManager.hpp"
+#include "RdMentalMap.hpp"
+#include "RdMockupRobotManager.hpp"
+#include "MockupAudioManager.hpp"
 
 #include <yarp/os/Network.h>
 
@@ -35,13 +43,43 @@ class InitStateTest : public testing::Test
 //--- Tests ------------------------------------------------------------------------------------------
 TEST_F(InitStateTest, InitStateWorksCorrectly )
 {
-    //-- Create mockup objects
+    //-- Register managers to be used:
+    MockupNetworkManager::RegisterManager();
+    RdMockupImageManager::RegisterManager();
+    MockupInputManager::RegisterManager();
+    MockupAudioManager::RegisterManager();
 
+    //-- Create managers
+    RdNetworkManager * networkManager = RdNetworkManager::getNetworkManager("MOCKUP");
+    MockupNetworkManager * mockupNetworkManager = dynamic_cast<MockupNetworkManager *>(networkManager);
+
+    RdImageManager * imageManager = RdImageManager::getImageManager("MOCKUP");
+    RdMockupImageManager * mockupImageManager = dynamic_cast<RdMockupImageManager *>(imageManager);
+
+    RdInputManager * inputManager = RdInputManager::getInputManager("MOCKUP");
+    MockupInputManager * mockupInputManager = dynamic_cast<MockupInputManager *>(inputManager);
+
+    AudioManager * audioManager = AudioManager::getAudioManager("MOCKUP");
+    MockupAudioManager * mockupAudioManager = dynamic_cast<MockupAudioManager *>(audioManager);
+
+    RdMentalMap * mentalMap = RdMentalMap::getMentalMap();
+
+    RdMockupRobotManager * mockupRobotManager = new RdMockupRobotManager(0);
 
     //-- Create fsm with InitState
     StateMachineBuilder builder;
     ASSERT_TRUE(builder.setDirectorType("YARP"));
 
-    int init_state_id = builder.addState(new InitState())
+    int init_state_id = builder.addState(new InitState());
+
+    //-- Delete things
+    RdNetworkManager::destroyNetworkManager();
+    networkManager = NULL;
+    mockupNetworkManager = NULL;
+    RdImageManager::destroyImageManager();
+    imageManager = NULL;
+    mockupImageManager = NULL;
+    RdInputManager::destroyInputManager();
+    AudioManager::destroyAudioManager();
 
 }
