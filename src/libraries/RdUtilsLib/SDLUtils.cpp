a01c67b7633d60c3ad3ea3177aaa9f6d30236b52
David.Estevez <david.estevez.fdez@gmail.com>
2016-04-03 04:03:27 +0200

    Compile error in SDLutils gone

---
+++ src/libraries/RdUtilsLib/SDLUtils.cpp
@@ -0,0 +1,23 @@
+#include "SDLUtils.hpp"
+
+SDL_Surface * rd::RdImage2SDLImage(rd::RdImage image)
+{
+    if (image.width()==0)
+        return NULL;
+
+    //-- Image parameters (in the future they could be parameters of this function
+    int cameraDepth=8;  // the depth of the surface in bits
+    int cameraChannels=3;  // R G B
+    int cameraWidthstep = image.getRowSize(); // the length of a row of pixels in bytes 1920 for 640x480
+    int cameraWidth = image.width();
+    int cameraHeight = image.height();
+
+
+    // http://stackoverflow.com/1questions/393954/how-to-convert-an-opencv-iplimage-to-an-sdl-surface
+    return SDL_CreateRGBSurfaceFrom( (void*)image.getRawImage(),
+                                         cameraWidth, cameraHeight,
+                                         cameraDepth*cameraChannels,
+                                         cameraWidthstep,
+                                         0x0000ff, 0x00ff00, 0xff0000, 0
+                                       );
+}
--- src/libraries/RdUtilsLib/SDLUtils.hpp
+++ src/libraries/RdUtilsLib/SDLUtils.hpp
@@ -6,26 +6,10 @@
 #include <RdImageManager.hpp>
 #include <SDL/SDL_image.h>
 
-SDL_Surface * RdImage2SDLImage(rd::RdImage image)
-{
-    if (image.width()==0)
-        return NULL;
+namespace rd {
 
-    //-- Image parameters (in the future they could be parameters of this function
-    int cameraDepth=8;  // the depth of the surface in bits
-    int cameraChannels=3;  // R G B
-    int cameraWidthstep = image.getRowSize(); // the length of a row of pixels in bytes 1920 for 640x480
-    int cameraWidth = image.width();
-    int cameraHeight = image.height();
+SDL_Surface * RdImage2SDLImage(RdImage image);
 
-
-    // http://stackoverflow.com/1questions/393954/how-to-convert-an-opencv-iplimage-to-an-sdl-surface
-    return SDL_CreateRGBSurfaceFrom( (void*)image.getRawImage(),
-                                         cameraWidth, cameraHeight,
-                                         cameraDepth*cameraChannels,
-                                         cameraWidthstep,
-                                         0x0000ff, 0x00ff00, 0xff0000, 0
-                                       );
 }
 
 #endif  // __SDL_UTILS_HPP__
