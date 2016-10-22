bc4c3417e81f848771ba2283c60072f1ac6d724d
David.Estevez <david.estevez.fdez@gmail.com>
2016-10-22 20:41:06 +0200

    Add unit test for MockupScreen class

---
+++ test/testMockupScreen.cpp
@@ -0,0 +1,36 @@
+/***
+ * testMockupScreen
+ *
+ * Testing MockupScreen class, which offers a generic Screen for testing purposes
+ *
+ * This test is NOT AUTOMATIC, not suitable for running it with a CI server
+ *
+ ***/
+
+#include <yarp/os/Time.h>
+
+#include "MockupScreen.hpp"
+
+using namespace rd;
+
+int main()
+{
+    //-- Initialization
+    initSDL();
+
+    MockupScreen screen;
+    screen.init();
+
+    //-- Setup message
+    screen.update(MockupScreen::PARAM_MESSAGE, "This is a test!");
+
+    //-- Show it for a few seconds
+    screen.show();
+    yarp::os::Time::delay(2);
+
+    //-- Clean stuff
+    screen.cleanup();
+    cleanupSDL();
+
+    return 0;
+}
