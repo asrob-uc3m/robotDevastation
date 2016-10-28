c81ec4895ffd8d8aa84492cdee5c38d57074e446
David.Estevez <david.estevez.fdez@gmail.com>
2016-10-28 17:42:02 +0200

    Update class Hub to take into account the new manager ScreenManager

--- src/libraries/RdUtilsLib/Hub.cpp
+++ src/libraries/RdUtilsLib/Hub.cpp
@@ -11,7 +11,8 @@ rd::ManagerHub::ManagerHub()
 
 rd::ManagerHub::ManagerHub(rd::RdNetworkManager *networkManager, rd::RdImageManager *imageManager,
                            rd::RdInputManager *inputManager, rd::RdMentalMap *mentalMap,
-                           rd::RdRobotManager *robotManager, rd::AudioManager *audioManager)
+                           rd::RdRobotManager *robotManager, rd::AudioManager *audioManager,
+                           ScreenManager *screenManager)
 {
     this->networkManager = networkManager;
     this->imageManager = imageManager;
@@ -19,6 +20,7 @@ rd::ManagerHub::ManagerHub(rd::RdNetworkManager *networkManager, rd::RdImageMana
     this->mentalMap = mentalMap;
     this->robotManager = robotManager;
     this->audioManager = audioManager;
+    this->screenManager = screenManager;
 }
 
 bool rd::ManagerHub::setNetworkManager(rd::RdNetworkManager *networkManager) { this->networkManager = networkManager;}
@@ -27,3 +29,4 @@ bool rd::ManagerHub::setInputManager(rd::RdInputManager *inputManager) { this->i
 bool rd::ManagerHub::setMentalMap(rd::RdMentalMap *mentalMap) { this->mentalMap = mentalMap;}
 bool rd::ManagerHub::setRobotManager(rd::RdRobotManager *robotManager) { this->robotManager = robotManager;}
 bool rd::ManagerHub::setAudioManager(rd::AudioManager *audioManager) { this->audioManager = audioManager;}
+bool rd::ManagerHub::setScreenManager(rd::ScreenManager *screenManager) {this->screenManager = screenManager;}
